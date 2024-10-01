545b91c3c428ef2c5db32d341b10305b14ee74c4
Laura Molina <ld.molina11@uniandes.edu.co>
2024-09-30 23:24:24 -0500

    :sparkles: adding colors theme

--- app/src/main/java/com/misoux/abcall/ui/theme/Theme.kt
+++ app/src/main/java/com/misoux/abcall/ui/theme/Theme.kt
@@ -10,49 +10,45 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = Blue700,
+    secondary = Blue500,
+    tertiary = Blue400,
+    background = Blue950,
+    surface = Blue900,
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onBackground = Blue50,
+    onSurface = Blue50
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Blue500,
+    secondary = Blue300,
+    tertiary = Blue200,
+    background = Blue50,
+    surface = Blue100,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Blue700,
+    onBackground = Blue900,
+    onSurface = Blue900
 )
 
 @Composable
 fun ABCallTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
