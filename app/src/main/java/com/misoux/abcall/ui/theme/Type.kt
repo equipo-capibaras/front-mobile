4f2093e3788092a0dfb0a5678492c8f8dea17698
Laura Molina <ld.molina11@uniandes.edu.co>
2024-10-01 21:23:02 -0500

    :sparkles: adding fira sans font

--- app/src/main/java/com/misoux/abcall/ui/theme/Type.kt
+++ app/src/main/java/com/misoux/abcall/ui/theme/Type.kt
@@ -2,19 +2,32 @@ package com.misoux.abcall.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.misoux.abcall.R
+
+val FiraSans = FontFamily(
+    Font(R.font.fira_sans_regular, FontWeight.Normal),
+    Font(R.font.fira_sans_semi_bold, FontWeight.SemiBold),
+    Font(R.font.fira_sans_light, FontWeight.Light),
+    Font(R.font.fira_sans_light_italic, FontWeight.Light, FontStyle.Italic)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FiraSans,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+        fontSize = 16.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FiraSans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
