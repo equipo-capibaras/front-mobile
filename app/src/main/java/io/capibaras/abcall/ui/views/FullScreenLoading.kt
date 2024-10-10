5cfa7d7388d77cfc09662907ba254ba44178d0db
Laura Molina <ld.molina11@uniandes.edu.co>
2024-10-09 23:37:37 -0500

    :sparkles: full screen loading

---
+++ app/src/main/java/io/capibaras/abcall/ui/views/FullScreenLoading.kt
@@ -0,0 +1,33 @@
+package io.capibaras.abcall.ui.views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+
+@Composable
+fun FullScreenLoading(isLoading: Boolean) {
+    if (isLoading) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White.copy(alpha = 0.8f))
+                .zIndex(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.primary,
+                strokeWidth = 4.dp,
+                modifier = Modifier.size(60.dp)
+            )
+        }
+    }
+}
