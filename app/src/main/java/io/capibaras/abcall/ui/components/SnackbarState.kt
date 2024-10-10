801aed79a7fd7343df71915ff1eba58789df63b7
Laura Molina <ld.molina11@uniandes.edu.co>
2024-10-10 00:30:48 -0500

    :bug: bot refactors

--- app/src/main/java/io/capibaras/abcall/ui/components/SnackbarState.kt
+++ app/src/main/java/io/capibaras/abcall/ui/components/SnackbarState.kt
@@ -1,7 +1,32 @@
 package io.capibaras.abcall.ui.components
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import io.capibaras.abcall.ui.theme.LocalCustomColors
+
 enum class SnackbarState {
     ERROR,
     SUCCESS,
     INFO
+}
+
+@Composable
+fun getSnackbarColors(state: SnackbarState): Pair<Color, Color> {
+    return when (state) {
+        SnackbarState.ERROR -> Pair(
+            MaterialTheme.colorScheme.error,
+            MaterialTheme.colorScheme.onError
+        )
+
+        SnackbarState.SUCCESS -> Pair(
+            LocalCustomColors.current.success,
+            LocalCustomColors.current.onSuccess
+        )
+
+        SnackbarState.INFO -> Pair(
+            MaterialTheme.colorScheme.primary,
+            MaterialTheme.colorScheme.onPrimary
+        )
+    }
 }
