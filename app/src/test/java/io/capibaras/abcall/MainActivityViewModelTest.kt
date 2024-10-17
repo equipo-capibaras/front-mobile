93f01b948d5f854a840c29798e6a405b2ee3f7b8
Laura Molina <ld.molina11@uniandes.edu.co>
2024-10-16 19:13:10 -0500

    :white_check_mark: unit test

--- app/src/test/java/io/capibaras/abcall/MainActivityViewModelTest.kt
+++ app/src/test/java/io/capibaras/abcall/MainActivityViewModelTest.kt
@@ -1,14 +1,20 @@
 package io.capibaras.abcall
 
+import io.capibaras.abcall.data.LogoutManager
 import io.capibaras.abcall.data.TokenManager
+import io.capibaras.abcall.data.repositories.UsersRepository
 import io.capibaras.abcall.viewmodels.MainActivityViewModel
 import io.mockk.MockKAnnotations
+import io.mockk.Runs
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.impl.annotations.MockK
+import io.mockk.just
 import io.mockk.verify
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
@@ -19,23 +25,35 @@ import org.junit.Test
 @OptIn(ExperimentalCoroutinesApi::class)
 class MainActivityViewModelTest {
     private lateinit var viewModel: MainActivityViewModel
+    private val logoutEventFlow = MutableStateFlow(false)
 
     @MockK
     private lateinit var tokenManager: TokenManager
 
+    @MockK
+    private lateinit var logoutManager: LogoutManager
+
+    @MockK
+    private lateinit var usersRepository: UsersRepository
+
     private val testDispatcher = StandardTestDispatcher()
+
 
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
         Dispatchers.setMain(testDispatcher)
+        coEvery { logoutManager.logoutEvent } returns logoutEventFlow
+        coEvery { tokenManager.clearAuthToken() } just Runs
+        coEvery { usersRepository.deleteUsers() } just Runs
+        coEvery { logoutManager.resetLogoutState() } just Runs
     }
 
     @Test
     fun `test user is logged in when token is available`() = runTest {
         coEvery { tokenManager.getAuthToken() } returns "valid_token"
 
-        viewModel = MainActivityViewModel(tokenManager)
+        viewModel = MainActivityViewModel(usersRepository, tokenManager, logoutManager)
 
         advanceUntilIdle()
 
@@ -49,7 +67,7 @@ class MainActivityViewModelTest {
     fun `test user is not logged in when token is null or empty`() = runTest {
         coEvery { tokenManager.getAuthToken() } returns ""
 
-        viewModel = MainActivityViewModel(tokenManager)
+        viewModel = MainActivityViewModel(usersRepository, tokenManager, logoutManager)
 
         advanceUntilIdle()
 
@@ -63,7 +81,7 @@ class MainActivityViewModelTest {
     fun `test user is not logged in when exception occurs`() = runTest {
         coEvery { tokenManager.getAuthToken() } throws Exception("Error getting token")
 
-        viewModel = MainActivityViewModel(tokenManager)
+        viewModel = MainActivityViewModel(usersRepository, tokenManager, logoutManager)
 
         advanceUntilIdle()
 
@@ -71,5 +89,22 @@ class MainActivityViewModelTest {
         assertEquals(true, viewModel.isSessionChecked)
 
         verify(exactly = 1) { tokenManager.getAuthToken() }
+    }
+
+    @Test
+    fun `test logout event triggers user data deletion`() = runTest {
+        coEvery { tokenManager.clearAuthToken() } returns Unit
+        coEvery { usersRepository.deleteUsers() } returns Unit
+        coEvery { logoutManager.resetLogoutState() } returns Unit
+
+        viewModel = MainActivityViewModel(usersRepository, tokenManager, logoutManager)
+
+        logoutEventFlow.emit(true)
+
+        advanceUntilIdle()
+
+        coVerify { tokenManager.clearAuthToken() }
+        coVerify { usersRepository.deleteUsers() }
+        coVerify { logoutManager.resetLogoutState() }
     }
 }
